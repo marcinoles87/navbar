3a277344bfa76143c3008ea6ad789fc31af7dad2
marcinoles87 <marcinoles87@gmail.com>
2023-03-03 21:07:25 +0100

    work with structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <Menu></Menu>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import ItemMenu from './ItemMenu'
+
+export default function Menu() {
+  return (
+    <div>
+        Menu
+        <ItemMenu></ItemMenu>
+    </div>
+  )
+}
