614ee866c6542816863620ba3000318163f84346
marcinoles87 <marcinoles87@gmail.com>
2023-03-04 14:50:15 +0100

    add css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,22 @@
+import React, { Component } from 'react'
 
-import React from 'react'
 import ItemMenu from './ItemMenu'
+import logo from './img/logo192.png'
 
 import './menu.css'
 
-export default function Menu() {
-  return (
-    <div className='menu-wrapper'>
-        <h1 className='menu-logo'>Logo</h1>
-        <ItemMenu></ItemMenu>
-    </div>
-  )
+
+export default class Menu extends Component {
+  render() {
+
+    return (
+      <div className='menu-wrapper'>
+      <h1 className='logo'>React<img src={logo}></img></h1>
+      
+      <ItemMenu></ItemMenu>
+  </div>
+    )
+  }
 }
+
+
